0aeb629c5cbac1678509418fed0ca7b52674eef9
Pokour <pokour@gmail.com>
2019-12-13 10:23:42 +0530

    updated landing page

--- src/app/component/landing/landing.component.ts
+++ src/app/component/landing/landing.component.ts
@@ -9,16 +9,34 @@ import Swiper from 'swiper';
 export class LandingComponent implements OnInit {
 
   describe = false;
-  roleDescription = ["You are our favourite people. If you are here to learn and access our free library, then giddy up, sign up and choose your role to be a Student.", 
-                      "If you are not a student anymore, but a learner and doer in life, then sign up and choose your role to be a Collaborator. You will get to work on our ongoing projects and will also have access to our library.", 
-                      "If your are here to check on how your students are doing, and how our platform can benefit you and your students, then do sign up and we shall get back to you as soon as we can."
-                    ];
-  
+  roleDescription = ["You are our favourite people. If you are here to learn and access our free library, then giddy up, sign up and choose your role to be a Student.",
+    "If you are not a student anymore, but a learner and doer in life, then sign up and choose your role to be a Collaborator. You will get to work on our ongoing projects and will also have access to our library.",
+    "If your are here to check on how your students are doing, and how our platform can benefit you and your students, then do sign up and we shall get back to you as soon as we can."
+  ];
+
 
   constructor() { }
 
   ngOnInit() {
-
+    var swiper = new Swiper('.blog-slider', {
+      spaceBetween: 30
+      , effect: 'fade'
+      , loop: true
+      , autoplay: {
+        delay: 2000
+        ,
+      }
+      , mousewheel: {
+        invert: false
+        ,
+      }
+      , // autoHeight: true,
+      pagination: {
+        el: '.blog-slider__pagination'
+        , clickable: true
+        ,
+      }
+    });
   }
 
 }
